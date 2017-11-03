fd5cd42a765a08ca809ded1cfb541788bb6fb4f5
wxaxiaoyao <765485868@qq.com>
2017-11-03 19:33:31 +0800

    xxx

--- start.sh
+++ start.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
 
+boot_file="loader"
+if [ -n "$2" ];then
+	boot_file="${2}_loader"
+fi
 
 start_server(){
-	npl -d nws/src/loader.lua
+	npl -d nws/src/${boot_file}.lua
 }
 
 stop_server(){
-	pid=`ps uax | grep -e "npl.*nws/src/loader" | grep -v grep | awk '{print $2}'`
+	pid=`ps uax | grep -e "npl.*nws/src/${boot_file}" | grep -v grep | awk '{print $2}'`
 	if [ ! -z $pid ]; then
 		kill -9 $((pid))
 	fi
 }
 
 restart_server(){
-	stop_server
+	stop_server 
 	start_server
 }
 
 main(){
+
 	if [ "$1" == "start" ]; then
 		echo "start webserver..."
-		start_server
+		start_server 
 	elif [ "$1" == "stop" ]; then
 		echo "stop webserver"...
-		stop_server
+		stop_server 
 	elif [ "$1" == "restart" ]; then
 		echo "restart webserver..."
-		restart_server
+		restart_server 
 	else
 		echo "restart webserver..."
-		restart_server
+		restart_server 
 	fi
 }
 
 main $@
+

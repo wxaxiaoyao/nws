4e7a4c8d6805e3fcdd661a442157a4d7c373747c
wxaxiaoyao <765485868@qq.com>
2017-11-17 18:34:16 +0800

    xx

--- npl_mod/nws/string.lua
+++ npl_mod/nws/string.lua
@@ -4,7 +4,11 @@ local string = string or {}
 function string.split(str, sep)
 	local list = {}
 
-	for word in string.gmatch(str, '([^' .. sep .. ']+)') do
+	if not (string.match(str, sep .. "$")) then
+		str = str .. sep
+	end
+
+	for word in string.gmatch(str, '([^' .. sep .. ']*)' .. sep) do
 		list[#list+1] = word
 	end
 
